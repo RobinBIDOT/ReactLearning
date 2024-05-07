0736befb551c993e0f132ace641ece7d48f40f14
Robin BIDOT <rbidot@hotmail.fr>
2024-05-07 10:38:31 +0200

    Exercices de la formation Skilleos React

---
+++ premier-projet/src/App.js
@@ -0,0 +1,65 @@
+import './App.css';
+import Membre from "./components/Membre";
+import Button from "./components/Button";
+import {Component} from "react";
+
+
+const famille = {
+    membre1: {
+        nom: 'Robin',
+        age: 31
+    },
+    membre2: {
+        nom: 'Carolina',
+        age: 27
+    },
+    membre3: {
+        nom: 'Camilla',
+        age: 0
+    },
+    membre4: {
+        nom: 'Mozart',
+        age: 2
+    },
+}
+
+class App extends Component {
+    state = {
+        famille
+    }
+
+    handleClick = (num) => {
+        const famille = { ... this.state.famille }
+        famille.membre1.age += num
+        famille.membre2.age += num
+        famille.membre3.age += num
+        famille.membre4.age += num
+        this.setState({ famille })
+    }
+  render() {
+    const {titre} =this.props
+    const { famille } = this.state
+    return (
+        <div className='App'>
+          <h1>{titre}</h1>
+          <Membre
+              age={famille.membre1.age}
+              nom={famille.membre1.nom}/>
+          <Membre
+              age={famille.membre2.age}
+              nom={famille.membre2.nom}/>
+          <Membre
+              age={famille.membre3.age}
+              nom={famille.membre3.nom}/>
+          <Membre
+              age={famille.membre4.age}
+              nom={famille.membre4.nom}>
+              <strong>Je suis un chien.</strong>
+          </Membre>
+            <Button
+            vieillir={() => this.handleClick(2)}/>
+        </div>
+    )
+  }
+}
+export default App;
---
+++ premier-projet/src/components/Membre.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from "react";
+
+const Membre = ({nom, age, children}) => {
+    return (
+        <Fragment>
+            <h2>{nom.toUpperCase()} : {age} ans</h2>
+            {children ? <p>{children}</p> : <Fragment />}
+        </Fragment>
+    )
+}
+
+export default Membre
